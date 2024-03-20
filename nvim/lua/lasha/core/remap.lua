46252db374588225756702e3e3f5ce1c99d14a48
LashaSD <lashakardava@gmail.com>
2024-03-21 00:38:47 +0400

    change font

--- nvim/lua/lasha/core/remap.lua
+++ nvim/lua/lasha/core/remap.lua
@@ -7,7 +7,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
+-- Pastes From Void Register and doesn't overwrite the copied value
+vim.keymap.set("x", "<leader>p", [["_dP]]) 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- nvim/lua/lasha/plugins/competitest.lua
+++ nvim/lua/lasha/plugins/competitest.lua
@@ -9,6 +9,8 @@ return {
     keys = {
       {"<leader>cl", "<cmd>CompetiTest receive testcases<cr>", desc = "Load Test Cases from the Browser" },
       {"<leader>ca", "<cmd>CompetiTest add_testcase<cr>", desc = "Add Test Cases Manually" },
+      {"<leader>ce", "<cmd>CompetiTest edit_testcase<cr>", desc = "Runs the Test Cases" },
+      {"<leader>cd", "<cmd>CompetiTest remove_testcase<cr>", desc = "Runs the Test Cases" },
       {"<leader>cr", "<cmd>CompetiTest run<cr>", desc = "Runs the Test Cases" },
     },
 }
