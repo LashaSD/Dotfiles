6f92008d07d42b4fc07ce37a546712a7060799ac
LashaSD <lashakardava@gmail.com>
2024-09-17 20:49:04 +0400

    Update mapping to <leader>keyword

--- .config/nvim/lua/lasha/core/remap.lua
+++ .config/nvim/lua/lasha/core/remap.lua
@@ -8,12 +8,12 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Pastes From Void Register and doesn't overwrite the copied value
-vim.keymap.set("x", "<leader>p", [["_dP]]) 
+vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- make w jumps PascalCase Sensitive
-vim.api.nvim_set_keymap('n', 'w', [[:call search('\u\l\|_\l', 'W')<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'b', [[:call search('\(\u\l\|_\l\)', 'bW')<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'e', [[:call search('\(\u\l\|_\l\)', 'eW')<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>w', [[:call search('\u\l\|_\l', 'W')<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>b', [[:call search('\(\u\l\|_\l\)', 'bW')<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>e', [[:call search('\(\u\l\|_\l\)', 'eW')<CR>]], { noremap = true, silent = true })
