df4a233efe9a0a89348850265e70e7a957316954
LashaSD <lashakardava@gmail.com>
2025-01-02 14:29:27 +0400

    Proper Descriptions for Harpoon Commands

--- .config/nvim/lua/lasha/plugins/harpoon.lua
+++ .config/nvim/lua/lasha/plugins/harpoon.lua
@@ -5,9 +5,9 @@ return {
     },
     keys = {
         {"<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Add File" },
-        {"<leader>hm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "harpoon menu" },
-        {"<leader>hh", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "file 1" },
-        {"<leader>hj", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", "file 2" },
-        {"<leader>hk", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", "file 3" },
+        {"<leader>hm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Open Harpoon Menu" },
+        {"<leader>hh", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Harpoon File 1" },
+        {"<leader>hj", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Harpoon File 2" },
+        {"<leader>hk", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Harpoon File 3" },
     },
 }
