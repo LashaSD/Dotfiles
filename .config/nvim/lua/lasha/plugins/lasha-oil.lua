34e324fcb15f6efac2675464fbf8d971d172339a
LashaSD <lashakardava@gmail.com>
2025-01-02 21:19:19 +0400

    Configure oil.nvim fork

---
+++ .config/nvim/lua/lasha/plugins/lasha-oil.lua
@@ -0,0 +1,35 @@
+return {
+    "LashaSD/oil.nvim",
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    config = function()
+        local oil = require('oil');
+        oil.setup({
+            watch_for_changes = true,
+            columns = {
+                "icon",
+                "size",
+                "git_status",
+            },
+            keymaps = {
+                ["<C-s>"] = "actions.select",
+                ["<C-v>"] = {"actions.select", opts = { vertical = true }},
+                ["<C-r>"] = "actions.refresh"
+            },
+            git_column = {
+                modified = {"~", "@character.special"},
+                staged = {"+", "@diff.plus"},
+                untracked = {"?", "@comment.warning"},
+                none = {"-", "@comment"}
+            },
+        });
+
+        -- Bind :Ex to Oil
+        vim.api.nvim_create_user_command('Ex', function() vim.cmd('Oil') end, {})
+
+        -- Bind :Vex to Oil
+        vim.api.nvim_create_user_command('Vex', function() vim.cmd('vsplit'); vim.cmd('Oil') end, { bang = true })
+
+        -- Optional: Bind :Sexplore to Oil in a horizontal split
+        vim.api.nvim_create_user_command('Sex', function() vim.cmd('split'); vim.cmd('Oil') end, { bang = true })
+    end
+}
