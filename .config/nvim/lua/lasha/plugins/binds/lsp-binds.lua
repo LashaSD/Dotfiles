97f9b5e5d0d0ac010b548b23ac6667f9771f2964
LashaSD <lashakardava@gmail.com>
2025-01-03 18:15:46 +0400

    feat: modularize lsp-binds

---
+++ .config/nvim/lua/lasha/plugins/binds/lsp-binds.lua
@@ -0,0 +1,49 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap;
+
+local function NextDiagnostic(opts_)
+    opts_ = opts_ or {}
+    return vim.diagnostic.jump(vim.tbl_deep_extend('force', opts_, {
+        count = 1;
+        float = true;
+    }));
+end
+
+local function PrevDiagnostic(opts_)
+    opts_ = opts_ or {}
+    return vim.diagnostic.jump(vim.tbl_deep_extend('force', opts_, {
+        count = -1;
+        float = true;
+    }));
+end
+
+local function CustomHover(opts_)
+    opts_ = opts_ or {};
+    return vim.lsp.buf.hover(vim.tbl_deep_extend('force', opts_, {
+        border = "rounded";
+    }));
+end
+
+
+local function on_attach(client, bufnr)
+    opts.buffer = bufnr
+
+    local function map(mode, bind, fun, desc)
+        opts.desc = desc;
+        keymap.set(mode, bind, fun, opts);
+    end
+
+    map("n", "<leader>gd", vim.lsp.buf.definition, "Go To Definition")
+    map("n", "<leader>gt", vim.lsp.buf.type_definition, "Go To Type Definition")
+    map("n", "<leader>rn", vim.lsp.buf.rename, "Smart Rename")
+    map("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", "Show Buffer Diagnostics")
+    map("n", "<leader>d", vim.diagnostic.open_float, "Show Line Diagnostics")
+    map("n", "[d", NextDiagnostic, "Jump To Next Diagnostic")
+    map("n", "]d", PrevDiagnostic, "Jump To Previous Diagnostic")
+    map("n", "K", CustomHover, "Show Docs For Symbols Under The Cursor")
+    map("n", "<leader>rs", ":LuauLsp restart<CR>", "Restart The LSP")
+end
+
+return {
+    on_attach = on_attach
+}
--- .config/nvim/lua/lasha/plugins/lsp/luau-lsp.lua
+++ .config/nvim/lua/lasha/plugins/lsp/luau-lsp.lua
@@ -5,63 +5,16 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        local keymap = vim.keymap;
-        local opts = { noremap = true, silent = true }
-        local on_attach = function(client, bufnr)
-			opts.buffer = bufnr
-
-			-- set keybinds
-			opts.desc = "Go To Definition"
-			keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts) -- show documentation for what is under cursor
-
-			opts.desc = "Go To Type Definition"
-			keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition, opts) -- show documentation for what is under cursor
-
-			opts.desc = "Smart rename"
-			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-			opts.desc = "Show buffer diagnostics"
-			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-			opts.desc = "Show line diagnostics"
-			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-			opts.desc = "Go to previous diagnostic"
-			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-			opts.desc = "Go to next diagnostic"
-			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-			opts.desc = "Show documentation for what is under cursor"
-			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-			opts.desc = "Restart LSP"
-			keymap.set("n", "<leader>rs", ":LuauLsp restart<CR>", opts) -- mapping to restart lsp if necessary
-		end
-
-        local capabilities = cmp_nvim_lsp.default_capabilities();
-
         local function rojo_project()
             return vim.fs.root(0, function(name)
                 return name:match ".+%.project%.json$"
             end)
         end
 
-        if rojo_project() then
-            vim.filetype.add {
-                extension = {
-                    lua = function(path)
-                        return path:match "%.nvim%.lua$" and "lua" or "luau"
-                    end,
-                },
-            }
-        end
-
 		require("luau-lsp").setup({
             server = {
-                capabilities = capabilities;
-                on_attach = on_attach;
+                capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                on_attach = require("lasha.plugins.binds.lsp-binds").on_attach,
                 settings = {
                   ["luau-lsp"] = {
                     completion = {
@@ -74,7 +27,7 @@ return {
             },
 			sourcemap = {
 				enabled = true,
-				autogenerate = true, -- automatic generation when the server is attached
+				autogenerate = true,
                 rojo_path = "rojo",
 				rojo_project_file = "default.project.json",
 			},
