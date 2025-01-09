496c84afa14c3c72bf9abb67d3c601c10a20f9d1
LashaSD <lashakardava@gmail.com>
2025-01-10 01:26:49 +0400

    reformat lsp so that mason is also configured in lsp-config

--- .config/nvim/lua/lasha/plugins/lsp/lsp-config.lua
+++ .config/nvim/lua/lasha/plugins/lsp/lsp-config.lua
@@ -6,6 +6,24 @@ return {
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 	},
 	config = function()
+        --Setup Mason
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+
+		mason.setup({})
+
+		mason_lspconfig.setup({
+            ensure_installed = {
+                "lua_ls",
+                "luau_lsp",
+                "clangd",
+                "ts_ls",
+                "cssls",
+                "omnisharp"
+			},
+			automatic_installation = true,
+		})
+
 		-- Change the Diagnostic symbols in the sign column (gutter)
 		local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 		for type, icon in pairs(signs) do
@@ -16,10 +34,6 @@ return {
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
         local on_attach = require("lasha.plugins.binds.lsp-binds").on_attach
         local lspconfig = require("lspconfig")
-        lspconfig["pyre"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        })
 
         lspconfig["clangd"].setup({
           capabilities = capabilities,
@@ -46,11 +60,18 @@ return {
 				},
 			},
 		})
+
         lspconfig["ts_ls"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
         });
+
         lspconfig["cssls"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+        });
+
+        lspconfig["omnisharp"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
         });
