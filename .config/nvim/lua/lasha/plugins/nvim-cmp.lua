5d0ba5d80fd1c73e108ead501bab0b104b08c452
LashaSD <lashakardava@gmail.com>
2025-02-14 18:27:34 +0400

    add borders to auto completion

--- .config/nvim/lua/lasha/plugins/nvim-cmp.lua
+++ .config/nvim/lua/lasha/plugins/nvim-cmp.lua
@@ -1,71 +1,81 @@
 return {
-  "hrsh7th/nvim-cmp",
-  version = false, -- last release is way too old
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-  },
-  opts = function()
-    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-    local cmp = require("cmp")
-    local defaults = require("cmp.config.default")()
-    return {
-      completion = {
-        completeopt = "menu,menuone,noinsert",
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            if #cmp.get_entries() == 1 then
-              cmp.confirm({ select = true })
-            else
-              cmp.select_next_item()
-            end
-          else
-            fallback();
-          end
-        end, { "i", "s" }),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-l>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping({
-          i = function(fallback)
-            if cmp.visible() and cmp.get_active_entry() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-            else
-              fallback()
-            end
-          end,
-          s = cmp.mapping.confirm({ select = true }),
-          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-        }),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "path" },
-      }, {
-        { name = "buffer" },
-      }),
-      experimental = {
-        ghost_text = {
-          hl_group = "CmpGhostText",
-        },
-      },
-      sorting = defaults.sorting,
-    }
-  end,
-  ---@param opts cmp.ConfigSchema
-  config = function(_, opts)
-    for _, source in ipairs(opts.sources) do
-      source.group_index = source.group_index or 1
-    end
-    require("cmp").setup(opts)
-  end,
+    "hrsh7th/nvim-cmp",
+    version = false,
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path"
+    },
+    opts = function()
+        vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+        local cmp = require("cmp")
+        local defaults = require("cmp.config.default")()
+        return {
+            window = {
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                    border = "single",
+                    winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder',
+                },
+                documentation = {
+                    border = "single",
+                    winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder',
+                }
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        if #cmp.get_entries() == 1 then
+                            cmp.confirm({ select = true })
+                        else
+                            cmp.select_next_item()
+                        end
+                    else
+                        fallback();
+                    end
+                end, { "i", "s" }),
+                ["<C-j>"] = cmp.mapping.select_next_item(),
+                ["<C-k>"] = cmp.mapping.select_prev_item(),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<C-l>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<CR>"] = cmp.mapping({
+                    i = function(fallback)
+                        if cmp.visible() and cmp.get_active_entry() then
+                            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                        else
+                            fallback()
+                        end
+                    end,
+                    s = cmp.mapping.confirm({ select = true }),
+                    c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+                }),
+            }),
+            sources = cmp.config.sources(
+                {
+                    { name = "nvim_lsp" },
+                    { name = "path" }
+                },
+                {
+                    { name = "buffer" }
+                }
+            ),
+            experimental = {
+                ghost_text = {
+                    hl_group = "CmpGhostText",
+                },
+            },
+            sorting = defaults.sorting,
+        }
+    end,
+    config = function(_, opts)
+        for _, source in ipairs(opts.sources) do
+            source.group_index = source.group_index or 1
+        end
+        require("cmp").setup(opts)
+    end,
 }
