2f885e9bf26d4eeea040cfec37ad46ea916465d5
LashaSD <lashakardava@gmail.com>
2025-04-25 01:35:45 +0400

    fix: restructure config

--- .config/nvim/lua/lasha/plugins/lsp/lsp-config.lua
+++ .config/nvim/lua/lasha/plugins/lsp/lsp-config.lua
@@ -1,79 +1,60 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-	},
-	config = function()
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        { "antosha417/nvim-lsp-file-operations", config = true },
+    },
+    config = function()
         --Setup Mason
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
 
-		mason.setup({})
+        mason.setup({})
 
-		mason_lspconfig.setup({
-            ensure_installed = {
-                "lua_ls",
-                "luau_lsp",
-                "clangd",
-                "ts_ls",
-                "cssls",
-                "omnisharp"
-			},
-			automatic_installation = true,
-		})
+        local lsp_ids = { "lua_ls", "luau_lsp", "clangd", "ts_ls", "cssls", "omnisharp", "rust_analyzer" };
+        mason_lspconfig.setup({
+            ensure_installed = lsp_ids,
+            automatic_installation = true,
+        })
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        -- Change the Diagnostic symbols in the sign column (gutter)
+        local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
         end
 
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
         local on_attach = require("lasha.plugins.binds.lsp-binds").on_attach
         local lspconfig = require("lspconfig")
 
-        lspconfig["clangd"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach
+        local lsp_blacklist = { "lua_ls", "luau_lsp" };
+        for _, value in ipairs(lsp_ids) do
+            if not vim.tbl_contains(lsp_blacklist, value) then
+                lspconfig[value].setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach
+                })
+            end
+        end
+
+        lspconfig["lua_ls"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        library = {
+                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                            [vim.fn.stdpath("config") .. "/lua"] = true,
+                        },
+                    },
+                },
+            },
         })
-
-		-- configure lua server (with special settings)
-		lspconfig["lua_ls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = { -- custom settings for lua
-				Lua = {
-					-- make the language server recognize "vim" global
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						-- make language server aware of runtime files
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-							[vim.fn.stdpath("config") .. "/lua"] = true,
-						},
-					},
-				},
-			},
-		})
-
-        lspconfig["ts_ls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        });
-
-        lspconfig["cssls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        });
-
-        lspconfig["omnisharp"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        });
-	end,
+    end,
 }
