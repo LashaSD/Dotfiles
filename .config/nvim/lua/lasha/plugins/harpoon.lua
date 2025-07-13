e1786394a7093ddb00855866b13681b3b2031723
LashaSD <lashakardava@gmail.com>
2025-07-13 17:12:26 +0400

    feat: add more binds for harpoon

--- .config/nvim/lua/lasha/plugins/harpoon.lua
+++ .config/nvim/lua/lasha/plugins/harpoon.lua
@@ -9,5 +9,10 @@ return {
         {"<leader>hh", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Harpoon File 1" },
         {"<leader>hj", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Harpoon File 2" },
         {"<leader>hk", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Harpoon File 3" },
+        {"<leader>hl", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "Harpoon File 3" },
+        {"<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", desc = "Harpoon File 3" },
+        {"<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", desc = "Harpoon File 3" },
+        {"<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", desc = "Harpoon File 3" },
+        {"<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", desc = "Harpoon File 3" },
     },
 }
