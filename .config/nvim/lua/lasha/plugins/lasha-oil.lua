ea9da34c83a8099a33b2e9d9dd1d12e7631c98dc
LashaSD <lashakardava@gmail.com>
2025-07-13 17:12:11 +0400

    fix: only use oil for smaller directories

--- .config/nvim/lua/lasha/plugins/lasha-oil.lua
+++ .config/nvim/lua/lasha/plugins/lasha-oil.lua
@@ -4,9 +4,10 @@ return {
     config = function()
         local oil = require('oil');
         oil.setup({
+            default_file_explorer = false,
             skip_confirm_for_simple_edits = true,
             delete_to_trash = true,
-            watch_for_changes = true,
+            watch_for_changes = false,
             columns = {
                 "git_status",
                 "icon",
@@ -29,13 +30,34 @@ return {
         });
 
         -- Bind :Ex to Oil
-        vim.api.nvim_create_user_command('Ex', function() vim.cmd('Oil') end, {})
+        -- vim.api.nvim_create_user_command('Ex', function() vim.cmd('Oil') end, {})
 
         -- Bind :Vex to Oil
-        vim.api.nvim_create_user_command('Vex', function() vim.cmd('vsplit'); vim.cmd('Oil') end, { bang = true })
+        -- vim.api.nvim_create_user_command('Vex', function() vim.cmd('vsplit'); vim.cmd('Oil') end, { bang = true })
 
         -- Optional: Bind :Sexplore to Oil in a horizontal split
-        vim.api.nvim_create_user_command('Sex', function() vim.cmd('split'); vim.cmd('Oil') end, { bang = true })
+        -- vim.api.nvim_create_user_command('Sex', function() vim.cmd('split'); vim.cmd('Oil') end, { bang = true })
+
+        vim.api.nvim_create_user_command("Ex", function(opts)
+            local path = opts.args ~= "" and vim.fn.expand(opts.args) or vim.fn.getcwd();
+            local MAX_ENTRIES = 250;
+            local count = 0;
+
+            -- Count directory entries up to max_entries+1
+            for _ in vim.fs.dir(path) do
+                count = count + 1;
+                if count > MAX_ENTRIES then break end
+            end
+
+            if count > MAX_ENTRIES then
+                vim.cmd.edit(path);
+            else
+                oil.open(path);
+            end
+        end, {
+                nargs = "?",
+                desc = "Smart Ex",
+            });
 
         vim.keymap.set("n", "<leader>of", ":Oil --float<CR>", { noremap = true, silent = true, desc = "Open Oil in float mode" })
     end
