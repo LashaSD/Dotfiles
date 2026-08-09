8f1d6b9b0135b0de08ab68b131378d752a1eb2fa
LashaSD <lashakardava@gmail.com>
2026-08-10 02:27:35 +0400

    add custom luau treesitter

--- .config/nvim/lua/lasha/plugins/treesitter.lua
+++ .config/nvim/lua/lasha/plugins/treesitter.lua
@@ -1,16 +1,29 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
     config = function()
-        local configs = require("nvim-treesitter.configs");
+        local ts = require("nvim-treesitter");
 
-        configs.setup({
-            ensure_installed = { "c", "lua", "luau", "vim", "vimdoc", "javascript", "html", "markdown" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+        ts.setup();
+        ts.install({ "c", "lua", "vim", "vimdoc", "javascript", "html", "markdown", "c_sharp" });
+
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                pcall(vim.treesitter.start, buf);
+            end,
+        });
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "TSUpdate",
+            callback = function()
+                local parser_config = require("nvim-treesitter.parsers");
+                parser_config.luau = {
+                    install_info = {
+                        url = "https://github.com/LashaSD/tree-sitter-luau",
+                        branch = "main",
+                        queries = "queries/neovim",
+                    },
+                };
+            end,
+        });
+    end,
 }
